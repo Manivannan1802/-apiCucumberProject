a00f925e274ee8c0e5d19e5c6d1228dd015630ba
Mani <sanmani460@gmail.com>
2024-03-03 01:09:42 +0530

    mani added API code

---
+++ src/main/java/apiCucumberProject/apiCucumberProject/App.java
@@ -0,0 +1,13 @@
+package apiCucumberProject.apiCucumberProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
